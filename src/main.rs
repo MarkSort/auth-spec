b546d6cb27468d3ec94bc69ff4a71447aed0a4d6
Mark Achee <mark@achee.com>
2020-03-01 22:19:23 -0600

    abstract requests

--- src/main.rs
+++ src/main.rs
@@ -2,137 +2,40 @@ use hyper::{Body, Method, Request, Response, StatusCode};
 
 #[tokio::main]
 async fn main() {
-    let mut c = Checks::new();
+    let mut c = Checks::new("http://localhost:8000".into());
 
-    let client = hyper::Client::new();
+    c.path = "/users";
 
-    c.group = "POST /users 400 - no content-length";
-    let response = client.request(
-        Request::builder()
-            .method(Method::POST)
-            .uri("http://localhost:3000/users")
-            //.header("content-type", "application/json")
-            //.body(Body::from(r#"{"library":"hyper"}"#))
-            .body(Body::empty())
-            .unwrap()
-    ).await.unwrap();
+    c.post_no_body("no content-length").await;
 
-    let status = response.status();
-    c.check(
-        status == StatusCode::BAD_REQUEST,
-        format!("should have returned 400 but returned {}", status),
-    );
-    let (passed, failed) = check_content_type(response);
-    c.bulk(passed, failed);
+    c.post_bad_content_type("content-type other than null or application/json").await;
 
-    
-    c.group = "POST /users 400 - content-type other than null or application/json";
-    let response = client.request(
-        Request::builder()
-            .method(Method::POST)
-            .uri("http://localhost:3000/users")
-            .header("content-type", "image/png")
-            .body(Body::empty())
-            .unwrap()
-    ).await.unwrap();
+    c.post("can't parse json", "not json".into(), StatusCode::BAD_REQUEST).await;
 
-    let status = response.status();
-    c.check(
-        status == StatusCode::BAD_REQUEST,
-        format!("should have returned 400 but returned {}", status),
-    );
-    let (passed, failed) = check_content_type(response);
-    c.bulk(passed, failed);
-
-
-    c.group = "POST /users 400 - can't parse json";
-    let response = client.request(
-        Request::builder()
-            .method(Method::POST)
-            .uri("http://localhost:3000/users")
-            .header("content-type", "application/json")
-            .body(Body::from("not json"))
-            .unwrap()
-    ).await.unwrap();
-
-    let status = response.status();
-    c.check(
-        status == StatusCode::BAD_REQUEST,
-        format!("should have returned 400 but returned {}", status),
-    );
-    let (passed, failed) = check_content_type(response);
-    c.bulk(passed, failed);
-
-
-    c.group = "POST /users 200 - correct response format";
-    let response = client.request(
-        Request::builder()
-            .method(Method::POST)
-            .uri("http://localhost:3000/users")
-            .header("content-type", "application/json")
-            .body(Body::from(r#"{"email":"test1234@example.com","password":"password"}"#))
-            .unwrap()
-    ).await.unwrap();
-
-    let status = response.status();
-    c.check(
-        status == StatusCode::OK,
-        format!("should have returned 200 but returned {}", status),
-    );
-    let (passed, failed) = check_content_type(response);
-    c.bulk(passed, failed);
+    c.post(
+        "correct response format",
+        r#"{"email":"test1234@example.com","password":"password"}"#.into(),
+        StatusCode::OK,
+    ).await;
 
     println!("{} Passed / {} Failed", c.passed, c.failed);
-}
-
-fn check_content_type(response: Response<Body>) -> (u16, Vec<String>) {
-    let mut passed = 0;
-    let mut failed: Vec<String> = vec!();
-    match response.headers().get("content-type") {
-        None => {
-            failed.push("missing content-type".into())
-        }
-        Some(content_type) => {
-            passed = passed + 1;
-
-            let content_type = content_type.to_str().unwrap();
-            let content_type_parts: Vec<&str> = content_type.split(';').collect();
-            if content_type_parts[0] == "application/json" {
-                passed = passed + 1;
-                if content_type_parts.len() == 2 && content_type_parts[1] == "encoding=utf8" {
-                    passed = passed + 1;
-                } else {
-                    failed.push("content-type missing 'encoding=utf8' or has too many parts".into());
-                }
-            } else {
-                failed.push(format!("content-type is '{}' instead of application/json", content_type));
-            }
-        }
-    }
-    (passed, failed)
 }
 
 struct Checks {
     passed: u16,
     failed: u16,
     group: &'static str,
+
+    base_url: String,
+
+    path: &'static str,
+    method: Method,
+    expect_json: bool,
+
+    client: hyper::Client<hyper::client::HttpConnector, Body>,
 }
 
 impl Checks {
-    /*
-    async fn sendRequest(&mut self, method: Method, path: String, group: &'static str) -> Response<Body> {
-        self.group = format!("{} {} {}", method, path, group);
-        self.client.request(
-            Request::builder()
-                .method(method)
-                .uri(format!("http://localhost:3000{}", path))
-                .header("content-type", "application/json")
-                .body(Body::from(r#"{"email":"test1234@example.com","password":"password"}"#))
-                .unwrap()
-        ).await.unwrap()
-    }
-    */
-
     fn check(&mut self, check: bool, description: String) -> bool {
         if check {
             self.pass(1);
@@ -148,18 +51,102 @@ impl Checks {
 
     fn fail(&mut self, description: String) {
         self.failed = self.failed + 1;
-        println!("Failed: {} - {}", self.group, description);
+        println!("Failed: {} {} - {} - {}", self.method, self.path, self.group, description);
     }
 
-    fn bulk(&mut self, passed: u16, failed: Vec<String>) {
-        self.pass(passed);
-        for error in failed {
-            self.fail(error);
+    async fn post_no_body(&mut self, group: &'static str) {
+        self.group = group;
+        self.method = Method::POST;
+        let response = self.client.request(
+            Request::builder()
+                .method(Method::POST)
+                .uri(format!("{}{}", self.base_url, self.path))
+                .body(Body::empty())
+                .unwrap()
+        ).await.unwrap();
+
+        self.check_response(response, StatusCode::BAD_REQUEST);
+    }
+
+    async fn post_bad_content_type(&mut self, group: &'static str) {
+        self.group = group;
+        self.method = Method::POST;
+        let response = self.client.request(
+            Request::builder()
+                .method(Method::POST)
+                .uri(format!("{}{}", self.base_url, self.path))
+                .header("content-type", "image/png")
+                .body(Body::empty())
+                .unwrap()
+        ).await.unwrap();
+
+        self.check_response(response, StatusCode::BAD_REQUEST);
+    }
+
+    async fn post(&mut self, group: &'static str, body: String, expected_status: StatusCode) {
+        self.group = group;
+        self.method = Method::POST;
+        let response = self.client.request(
+            Request::builder()
+                .method(Method::POST)
+                .uri(format!("{}{}", self.base_url, self.path))
+                .header("content-type", "application/json")
+                .body(Body::from(body))
+                .unwrap()
+        ).await.unwrap();
+
+        self.check_response(response, expected_status);
+    }
+
+    fn check_response(&mut self, response: Response<Body>, expected_status: StatusCode) {
+        let status = response.status();
+        self.check(
+            status == expected_status,
+            format!("should have returned 400 but returned {}", status),
+        );
+
+        if self.expect_json {
+            self.check_json_content_type(response);
         }
     }
 
-    fn new() -> Checks {
-        Checks { passed: 0, failed: 0, group: "" }
+    fn check_json_content_type(&mut self, response: Response<Body>) {
+        match response.headers().get("content-type") {
+            None => {
+                self.fail("missing content-type".into());
+            }
+            Some(content_type) => {
+                self.pass(1);
+
+                let content_type = content_type.to_str().unwrap();
+                let content_type_parts: Vec<&str> = content_type.split(';').collect();
+
+                if self.check(
+                    content_type_parts[0] == "application/json",
+                    format!("content-type is '{}' instead of application/json", content_type),
+                ) {
+                    self.check(
+                        content_type_parts.len() == 2 && content_type_parts[1] == "encoding=utf8",
+                        "content-type missing 'encoding=utf8' or has too many parts".into(),
+                    );
+                }
+            }
+        }
+    }
+
+
+    fn new(base_url: String) -> Checks {
+        Checks {
+            passed: 0,
+            failed: 0,
+            group: "",
+
+            base_url,
+            path: "/",
+            method: Method::GET,
+            expect_json: true,
+
+            client: hyper::Client::new(),
+        }
     }
 }
-
